d3a54265b8f27efade822e1b87819aac10e2b57f
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-08 12:03:43 +0200

    finished audio player

--- script/main.js
+++ script/main.js
@@ -10,3 +10,26 @@ hamburger.addEventListener('click', () => {
 menuBurger.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+
+import * as audioModule from './modules/audioPlayer.js';
+let player = document.querySelector('#player');
+let play = document.querySelector('#play');
+let interval;
+window.onload = () => {
+    audioModule.updateAudio();
+    player.volume = .5;
+    play.addEventListener('click', () => {
+        if (player.paused) {
+            interval = setInterval(audioModule.updateAudio, 1000);
+            player.play();
+            play.children[0].classList.remove('fa-play');
+            play.children[0].classList.add('fa-pause');
+        } else {
+            clearInterval(interval);
+            player.pause();
+            play.children[0].classList.remove('fa-pause');
+            play.children[0].classList.add('fa-play');
+        }
+    });
+}
