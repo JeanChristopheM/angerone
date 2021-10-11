063218873b4a6da62ae15b728fecaef77e536375
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-11 15:42:49 +0200

    polished tickets section

--- script/modules/carousel.js
+++ script/modules/carousel.js
@@ -1,16 +1,29 @@
 const initialize = () => {
-    let carousel = document.querySelector('.main__partners__logos__ulsponsors');
-    let carouselButtons = document.querySelector('.dotz.partners');
-    carouselButtons.addEventListener('click', (e) => {
+    /* Sponsors carousel */
+    let sponsorCarousel = document.querySelector('.main__partners__logos__ulsponsors');
+    let sponsorCarouselButtons = document.querySelector('.dotz.partners');
+    sponsorCarouselButtons.addEventListener('click', (e) => {
         let target = e.target;
         if (target.nodeName === "LI") {
             let index = target.id.slice(-1, target.id.length);
-            carousel.children[index - 1].scrollIntoView({ block: "nearest", inline: "start" });
+            sponsorCarousel.children[index - 1].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+        }
+    });
+    /* Tickets tickets */
+    let ticketsCarousel = document.querySelector('.ticket_flex');
+    let ticketsCarouselButtons = document.querySelector('.dotz.tickets');
+    ticketsCarouselButtons.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target.nodeName === "LI") {
+            let index = target.id.slice(-1, target.id.length);
+            ticketsCarousel.children[index - 1].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
         }
     });
 }
 const reset = () => {
-    let carousel = document.querySelector('.main__partners__logos__ulsponsors');
-    carousel.children[0].scrollIntoView({ block: "nearest", inline: "start" });
+    let sponsorCarousel = document.querySelector('.main__partners__logos__ulsponsors');
+    sponsorCarousel.children[0].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+    let ticketsCarousel = document.querySelector('.ticket_flex');
+    ticketsCarousel.children[0].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
 }
 export { initialize, reset };
