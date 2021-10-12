df3b0a93af6191fe13c53887ef949a21ed57d924
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-12 12:22:13 +0200

    added active fuctionality to carousels

--- script/modules/carousel.js
+++ script/modules/carousel.js
@@ -5,6 +5,10 @@ const initialize = () => {
     if (sponsorCarouselButtons) {
         sponsorCarouselButtons.addEventListener('click', (e) => {
             let target = e.target;
+            for (let item of sponsorCarouselButtons.children) {
+                item.classList.remove('active');
+            }
+            target.classList.add('active');
             if (target.nodeName === "LI") {
                 let index = target.id.slice(-1, target.id.length);
                 sponsorCarousel.children[index - 1].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
@@ -17,9 +21,29 @@ const initialize = () => {
     if (ticketsCarouselButtons) {
         ticketsCarouselButtons.addEventListener('click', (e) => {
             let target = e.target;
+            for (let item of ticketsCarouselButtons.children) {
+                item.classList.remove('active');
+            }
+            target.classList.add('active');
             if (target.nodeName === "LI") {
                 let index = target.id.slice(-1, target.id.length);
                 ticketsCarousel.children[index - 1].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+            }
+        });
+    }
+    /* Tour carousel */
+    let tourCarousel = document.querySelector('.main__tour__news');
+    let tourCarouselButtons = document.querySelector('.main__tour__calendar__list');
+    if (tourCarouselButtons) {
+        tourCarouselButtons.addEventListener('click', (e) => {
+            let target = e.target;
+            for (let item of tourCarouselButtons.children) {
+                item.classList.remove('active');
+            }
+            target.classList.add('active');
+            if (target.nodeName === "LI") {
+                let index = target.id.slice(-1, target.id.length);
+                tourCarousel.children[index - 1].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" });
             }
         });
     }
@@ -29,5 +53,13 @@ const reset = () => {
     //sponsorCarousel.children[0].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
     let ticketsCarousel = document.querySelector('.ticket_flex');
     //ticketsCarousel.children[0].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+    let tourCarousel = document.querySelector('.main__tour__news');
+    let tourCarouselButtons = document.querySelector('.main__tour__calendar__list');
+    /* tourCarousel.children[0].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" });
+    for (let item of tourCarouselButtons.children) {
+        item.classList.remove('active');
+    }
+    tourCarouselButtons.children[0].classList.add('active'); */
+
 }
 export { initialize, reset };
