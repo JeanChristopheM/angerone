4151829e45dcfc47d9f3ab873c82c64d2a53343a
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-10-12 15:30:36 +0200

    newz dotz not finished

--- script/modules/carousel.js
+++ script/modules/carousel.js
@@ -23,11 +23,25 @@ const initialize = () => {
             }
         });
     }
+    /* News caroussel */
+    let newsCarousel = document.querySelector('.container');
+    let newsCarouselButtons = document.querySelector('.dotz.news');
+    if (newsCarouselButtons) {
+        newsCarouselButtons.addEventListener('click', (e) => {
+            let target = e.target;
+            if (target.nodeName === "LI") {
+                let index = target.id.slice(-1, target.id.length);
+                newsCarousel.children[index - 1].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+            }
+        });
+    }
 }
 const reset = () => {
     let sponsorCarousel = document.querySelector('.main__partners__logos__ulsponsors');
     //sponsorCarousel.children[0].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
     let ticketsCarousel = document.querySelector('.ticket_flex');
     //ticketsCarousel.children[0].scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+    let newsCarousel = document.querySelector('.container');
+    //...................exact!
 }
 export { initialize, reset };
